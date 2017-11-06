15ea17c30ede3c21a34b761756292a59972b50a6
david <davidtnfsh@gmail.com>
2017-11-06 17:33:57 +0800

    [Bug fix] sys.argv length error + add try catch to show exception

--- WikiCategories/wiki.py
+++ WikiCategories/wiki.py
@@ -12,53 +12,66 @@ def genUrl(category):
     return 'https://zh.wikipedia.org/wiki/Category:' + category
 
 def dfs(root):
-    visited, stack = set(root), [root]
-    while stack:
-        result = defaultdict(list)
-        reverseResult = defaultdict(list)
-        
-        parent = stack.pop()
-        res = BeautifulSoup(requests.get(genUrl(parent)).text)
-        # node
-        for candidateOffsprings in res.select('.CategoryTreeLabelCategory'):
-            tradText = openCC.convert(candidateOffsprings.text).replace('/', '-')
+    if os.path.isfile('stack_visited.json'):
+        f = json.load(open('wikiCategoryJson/stack_visited.json', 'r'))
+        visited, stack = f['visited'], f['stack']
+    else:
+        visited, stack = set(root), [root]
 
-            # if it's a node hasn't been through
-            # append these res to stack
-            if tradText not in visited:
-                visited.add(tradText)
-                stack.append(tradText)
+    try:
+        while stack:
+            result = defaultdict(list)
+            reverseResult = defaultdict(list)
+            
+            parent = stack.pop()
+            res = BeautifulSoup(requests.get(genUrl(parent)).text)
+            # node
+            for candidateOffsprings in res.select('.CategoryTreeLabelCategory'):
+                tradText = openCC.convert(candidateOffsprings.text).replace('/', '-')
 
-                # build dictionary
-                result[parent].append(tradText)
-                reverseResult[tradText].append(parent)
+                # if it's a node hasn't been through
+                # append these res to stack
+                if tradText not in visited:
+                    visited.add(tradText)
+                    stack.append(tradText)
 
-        if os.path.isfile(parent + '.json'):
-            print('skip ' + parent)
-            continue
-        # 下一頁
-        leafNodeList = [res.select('#mw-pages a')]
-        while leafNodeList:
-            current = leafNodeList.pop(0)
+                    # build dictionary
+                    result[parent].append(tradText)
+                    reverseResult[tradText].append(parent)
 
-            # notyet 變數的意思是，因為wiki會有兩個下一頁的超連結
-            # 頂部跟底部
-            # 所以如果把頂部的bs4結果append到leafNodeLIst的話
-            # 底部就不用重複加
-            notyet = True
-            for child in current:
-                tradChild = openCC.convert(child.text).replace('/', '-')
-                if notyet and tradChild == '下一頁' and child.has_attr('href'):
-                    notyet = False
-                    leafNodeList.append(BeautifulSoup(requests.get(wikiBaseUrl + child['href']).text).select('#mw-pages a'))
-                else:
-                    if tradChild != '下一頁' and tradChild != '上一頁':
-                        result[parent].append(tradChild)
-                        reverseResult[tradChild].append(parent)
+            if os.path.isfile(parent + '.json'):
+                print('skip ' + parent)
+                continue
+            # 下一頁
+            leafNodeList = [res.select('#mw-pages a')]
+            while leafNodeList:
+                current = leafNodeList.pop(0)
 
-        # dump
-        json.dump(result, open('{}.json'.format(parent), 'w', encoding='utf-8'))
-        json.dump(reverseResult, open('{}.reverse.json'.format(parent), 'w', encoding='utf-8'))
-        gc.collect()
+                # notyet 變數的意思是，因為wiki會有兩個下一頁的超連結
+                # 頂部跟底部
+                # 所以如果把頂部的bs4結果append到leafNodeLIst的話
+                # 底部就不用重複加
+                notyet = True
+                for child in current:
+                    tradChild = openCC.convert(child.text).replace('/', '-')
+                    if notyet and tradChild == '下一頁' and child.has_attr('href'):
+                        notyet = False
+                        leafNodeList.append(BeautifulSoup(requests.get(wikiBaseUrl + child['href']).text).select('#mw-pages a'))
+                    else:
+                        if tradChild != '下一頁' and tradChild != '上一頁':
+                            result[parent].append(tradChild)
+                            reverseResult[tradChild].append(parent)
 
-dfs(sys.argv[1] if len(sys.argv) else root)
+            # dump
+            json.dump(result, open('wikiCategoryJson/{}.json'.format(parent), 'w', encoding='utf-8'))
+            json.dump(reverseResult, open('wikiCategoryJson/{}.reverse.json'.format(parent), 'w', encoding='utf-8'))
+            json.dump({'stack':stack, 'visited':list(visited)}, open('wikiCategoryJson/stack_visited.json', 'w', encoding='utf-8'))
+            gc.collect()
+    except Exception as e:
+        print('==============================')
+        print(parent)
+        print(str(e))
+        print('==============================')
+        raise e
+
+dfs(sys.argv[1] if len(sys.argv)>2 else root)
