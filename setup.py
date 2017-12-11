231269fa8fcf91da515e470a088dd3469c8a2947
david <davidtnfsh@gmail.com>
2017-12-11 11:31:46 +0800

    update readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['udicOpenData'],
     package_dir={'udicOpenData':'udicOpenData'},
     package_data={'udicOpenData':['dictionary/*', 'stopwords/*']},
-    version = '0.1',
+    version = '0.2',
     description = 'udic dictionary, stopwords module',
     author = ['davidtnfsh'],
     author_email = 'davidtnfsh@gmail.com',
