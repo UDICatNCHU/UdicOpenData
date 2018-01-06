2e1010db4b43bf10f568d301ebd19014540b2ce8
david <davidtnfsh@gmail.com>
2018-01-06 17:10:18 +0800

    add is_english fucntion to filter out short english word

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=['udicOpenData'],
     package_dir={'udicOpenData':'udicOpenData'},
     package_data={'udicOpenData':['dictionary/*', 'stopwords/*']},
-    version = '0.5',
+    version = '0.6',
     description = 'udic dictionary, stopwords module',
     author = ['davidtnfsh'],
     author_email = 'davidtnfsh@gmail.com',
--- udicOpenData/stopwords/__init__.py
+++ udicOpenData/stopwords/__init__.py
@@ -5,7 +5,7 @@ DIR_NAME = os.path.dirname(os.path.abspath(__file__))
 
 STOPWORD_JSON = json.load(open(os.path.join(DIR_NAME, 'stopwords.json'), 'r'))
 
-def rmsw(doc, flag=None):
+def rmsw(doc, flag=None, chineseOnly=False):
     def is_chinese(keyword):
         for uchar in keyword:
             if '\u4e00' <= uchar<='\u9fff':
@@ -13,9 +13,16 @@ def rmsw(doc, flag=None):
             else:
                 return False
         return True
-    doc = re.sub(r"[http|www]\S+", "", doc)
+    def is_english(keyword):
+        if not is_chinese(keyword) and keyword.isalpha():
+            return True
+        return False
     doc = doc.strip()
     if flag:
-        return (i.word for i in pseg.cut(doc) if i.flag.startswith(flag) and i.word not in STOPWORD_JSON and is_chinese(i.word) and i.word not in ['\xa0', '\xc2'])
+        tmp = (i for i in pseg.cut(doc) if i.flag.startswith(flag) and i.word not in STOPWORD_JSON and i.word not in ['\xa0', '\xc2'] and i.word.isdigit() == False and len(i.word) > 1 and (is_chinese(i.word) or (is_english(i.word) and len(i.word) >= 5)))
+        if chineseOnly:
+            return (i.word for i in tmp if is_chinese(i.word))
+        else:            
+            return (i.word for i in tmp)
     else:
         return (i for i in jieba.cut(doc) if i not in STOPWORD_JSON and is_chinese(i) and i not in ['\xa0', '\xc2'])
