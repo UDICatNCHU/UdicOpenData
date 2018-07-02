4ef87d15150ea2df36b9262dd00aef174d2d91d1
david <davidtnfsh@gmail.com>
2018-07-02 16:04:46 +0800

    [Bug fix] forgot to use utf-8 encoding

--- udicOpenData/stopwords/__init__.py
+++ udicOpenData/stopwords/__init__.py
@@ -3,7 +3,7 @@ import jieba.posseg as pseg
 import json, os, re
 DIR_NAME = os.path.dirname(os.path.abspath(__file__))
 
-STOPWORD_JSON = json.load(open(os.path.join(DIR_NAME, 'stopwords.json'), 'r'))
+STOPWORD_JSON = json.load(open(os.path.join(DIR_NAME, 'stopwords.json'), 'r', encoding='utf-8'))
 
 def rmsw(doc, flag=False):
     '''
