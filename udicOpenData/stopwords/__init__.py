6c6dfcf7d3c84955bf74bd8ce642aa73f2ce79e1
david <davidtnfsh@gmail.com>
2018-08-14 11:00:22 +0800

    refactor

--- udicOpenData/stopwords/__init__.py
+++ udicOpenData/stopwords/__init__.py
@@ -8,8 +8,7 @@ from pathlib import Path, PurePath
 import nltk
 
 STOPWORD_PKL = pickle.load(open(str(PurePath(Path(__file__).resolve().parent, 'stopwords.pkl')), 'rb'))
-STOPWORD_EN_PKL = pickle.load(open(str(PurePath(Path(__file__).resolve().parent, \
-'stopwords-en.pkl')), 'rb'))
+STOPWORD_EN_PKL = pickle.load(open(str(PurePath(Path(__file__).resolve().parent, 'stopwords-en.pkl')), 'rb'))
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -19,6 +18,7 @@ nltk.download('words')
 def rmsw(doc, flag=False):
     '''
     parameter:
+      doc: input string
       flag: boolean, if true will return segment with pos.
     '''
     import udicOpenData.dictionary
@@ -57,6 +57,11 @@ def rmsw(doc, flag=False):
 
 # Yang, 2018/08/08
 def rmsw_en(doc, flag=False):
+    '''
+    parameter:
+      doc: input string
+      flag: boolean, if true will return segment with pos.
+    '''
     def has_numbers(input_string):
         return any(char.isdigit() for char in input_string)
 
@@ -64,22 +69,16 @@ def rmsw_en(doc, flag=False):
     from nltk import ne_chunk, pos_tag, word_tokenize
 
     doc = doc.strip()
-
-    if flag:
-        chunks = ne_chunk(pos_tag(word_tokenize(doc)))
-        words = [w[0] if isinstance(w, tuple) else ' '.join(t[0] for t in w) for w in chunks]
-        for word in words:
-            word = re.sub(r'[^a-zA-Z0-9 -]', '', word)
-            if word and not has_numbers(word) and word.lower() not in STOPWORD_EN_PKL:
+    chunks = ne_chunk(pos_tag(word_tokenize(doc)))
+    words = [w[0] if isinstance(w, tuple) else ' '.join(t[0] for t in w) for w in chunks]
+    for word in words:
+        word = re.sub(r'[^a-zA-Z0-9 -]', '', word)
+        if word and not has_numbers(word) and word.lower() not in STOPWORD_EN_PKL:
+            if flag:
                 if len(word.split()) > 1:
                     pos = '/'.join([pos_tag(word_tokenize(i))[0][1] for i in word.split()])
                 else:
                     pos = pos_tag(word_tokenize(word))[0][1]
                 yield word, pos
-    else:
-        chunks = ne_chunk(pos_tag(word_tokenize(doc)))
-        words = [w[0] if isinstance(w, tuple) else ' '.join(t[0] for t in w) for w in chunks]
-        for word in words:
-            word = re.sub(r'[^a-zA-Z0-9 -]', '', word)
-            if word and not has_numbers(word) and word.lower() not in STOPWORD_EN_PKL:
+            else:
                 yield word
